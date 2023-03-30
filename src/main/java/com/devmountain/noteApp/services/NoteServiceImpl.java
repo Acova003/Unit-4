52362685d70f3b6cea685743cd7df2a1b6f27293
Amee <ameecova@yahoo.com>
2023-03-31 01:55:06 +0200

    Create addNote method

--- src/main/java/com/devmountain/noteApp/services/NoteServiceImpl.java
+++ src/main/java/com/devmountain/noteApp/services/NoteServiceImpl.java
@@ -1,9 +1,15 @@
 package com.devmountain.noteApp.services;
 
+import com.devmountain.noteApp.dtos.NoteDto;
+import com.devmountain.noteApp.entites.Note;
+import com.devmountain.noteApp.entites.User;
 import com.devmountain.noteApp.repositories.NoteRepository;
 import com.devmountain.noteApp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class NoteServiceImpl {
@@ -11,5 +17,14 @@ public class NoteServiceImpl {
     private UserRepository userRepository;
     @Autowired
     private NoteRepository noteRepository;
+
+    @Transactional
+    public void addNote(NoteDto noteDto, Long userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        Note note = new Note(noteDto);
+        userOptional.ifPresent(note::setUser);
+        noteRepository.saveAndFlush(note);
+
+    }
     
 }
