ce9d3e30e184bf97c0ebaf8587d1c57a50f0b39d
Amee <ameecova@yahoo.com>
2023-03-31 15:27:54 +0200

    Clean project

--- src/main/java/com/devmountain/noteApp/services/NoteServiceImpl.java
+++ src/main/java/com/devmountain/noteApp/services/NoteServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 @Service
 public class NoteServiceImpl {
@@ -40,6 +42,23 @@ public class NoteServiceImpl {
             note.setBody(noteDto.getBody());
             noteRepository.saveAndFlush(note);
         });
+    }
+
+    public List<NoteDto> getAllNotesByUserId(Long userId){
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()){
+            List<Note> noteList = noteRepository.findAllByUserEquals(userOptional.get());
+            return noteList.stream().map(note -> new NoteDto(note)).collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
+
+    public Optional<NoteDto> getNoteById(Long noteId) {
+        Optional<Note> noteOptional = noteRepository.findById(noteId);
+        if (noteOptional.isPresent()){
+            return Optional.of(new NoteDto(noteOptional.get()));
+        }
+        return Optional.empty();
     }
     
 }
