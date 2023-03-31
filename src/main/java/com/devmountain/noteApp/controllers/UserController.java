196334b0481b48e00f790daa6dac21513bd1a7de
Amee <ameecova@yahoo.com>
2023-03-31 16:10:57 +0200

    Set the base URL path for the controller

--- src/main/java/com/devmountain/noteApp/controllers/UserController.java
+++ src/main/java/com/devmountain/noteApp/controllers/UserController.java
@@ -1,10 +1,17 @@
 package com.devmountain.noteApp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devmountain.noteApp.services.UserService;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-    
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 }
