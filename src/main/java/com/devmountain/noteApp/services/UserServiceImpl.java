5813029ffacedceeb742a350d67d472195f7af20
Amee <ameecova@yahoo.com>
2023-04-04 00:44:09 +0200

    Add redirect url

--- src/main/java/com/devmountain/noteApp/services/UserServiceImpl.java
+++ src/main/java/com/devmountain/noteApp/services/UserServiceImpl.java
@@ -26,7 +26,7 @@ public class UserServiceImpl implements UserService{
         List<String> response = new ArrayList<>();
         User user = new User(userDto);
         userRepository.saveAndFlush(user);
-        response.add("User Added Successfully");
+        response.add("http://localhost:8080/login.html");
         return response;
     }
 
